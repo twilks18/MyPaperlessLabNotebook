33dbcefe3825838e6fac1332dc740a95ba8406c5
TaNeisha Wilks <twilks18@gmail.com>
2017-10-11 18:39:29 -0500

    changed introductioncontroller to experiment;added procedure and observations

--- src/main/java/org/tlw/MyPaperless/controllers/ExperimentController.java
+++ src/main/java/org/tlw/MyPaperless/controllers/ExperimentController.java
@@ -7,19 +7,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.tlw.MyPaperless.models.IntroductionModel;
+import org.tlw.MyPaperless.models.ProcObsModel;
 import org.tlw.MyPaperless.models.ReagentModel;
 
 import java.util.ArrayList;
 
 @Controller
-@RequestMapping(value = "intro")
-public class IntroductionController {
+@RequestMapping(value = "experiment")
+public class ExperimentController {
 
     ArrayList<IntroductionModel> intros = new ArrayList<IntroductionModel>();
     ArrayList<ReagentModel> reagents = new ArrayList<ReagentModel>();
+    ArrayList<ProcObsModel> procobs = new ArrayList<ProcObsModel>();
 
     // final page for intro and reagent contents
-    @RequestMapping(value = "")
+    @RequestMapping(value = "intro")
     public String introPage(Model model){
         model.addAttribute("intros",intros);
         model.addAttribute("reagents",reagents);
@@ -59,5 +61,22 @@ public class IntroductionController {
         reagents.add(reagent);
         model.addAttribute("reagents",reagents);
         return "section/reagentForm";
+    }
+
+    //form for procedure and observations
+    @RequestMapping(value = "procAndObsPage",method = RequestMethod.GET)
+    public String procAndObsForm(){
+        return "section/procAndObsForm";
+    }
+
+    @RequestMapping(value = "procAndObsPage", method = RequestMethod.POST)
+    public String processProcAndObsForm( @RequestParam String procedure, @RequestParam String observations,Model model){
+
+        ProcObsModel procob = new ProcObsModel(procedure,observations);
+        procobs.add(procob);
+
+        model.addAttribute("procobs", procobs);
+
+        return "section/procAndObsPage";
     }
 }
