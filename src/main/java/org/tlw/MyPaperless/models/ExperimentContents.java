29e997f83f7b481ef1dae7d2257664c682eeb67b
TaNeisha Wilks <twilks18@gmail.com>
2017-10-27 17:37:46 -0500

    Added remove method and id key

--- src/main/java/org/tlw/MyPaperless/models/ExperimentContents.java
+++ src/main/java/org/tlw/MyPaperless/models/ExperimentContents.java
@@ -5,58 +5,90 @@ import java.util.ArrayList;
 public class ExperimentContents {
 
 
-    public static ArrayList<IntroductionModel> intros = new ArrayList<>();
-    public static ArrayList<ReagentModel> reagents = new ArrayList<>();
-    public static ArrayList<ProcObsModel> procobs = new ArrayList<>();
-    public static ArrayList<ConclusionModel> conclusions = new ArrayList<>();
+    public static ArrayList<Intro> intros = new ArrayList<>();
+    public static ArrayList<Reagent> reagents = new ArrayList<>();
+    public static ArrayList<ProcObs> procobs = new ArrayList<>();
+    public static ArrayList<Conclusion> conclusions = new ArrayList<>();
 
     //add intro
-    public static void addIntro(IntroductionModel newIntro){
+    public static void addIntro(Intro newIntro){
         intros.add(newIntro);
     }
 
     //get all intros
-    public static ArrayList<IntroductionModel> getAllIntros(){
+    public static ArrayList<Intro> getAllIntros(){
         return intros;
     }
 
     //remove
+    public static void removeTitle(int id){
+        Intro titleToRemove = getTitleById(id);
+        intros.remove(titleToRemove);
+    }
 
+    //getById (titleId)
 
-    //getById
+    public static Intro getTitleById(int id) {
+
+        Intro titleid = null;
+
+        for (Intro theTitle : intros) {
+            if (theTitle.getTitleID() == id) {
+                titleid = theTitle;
+            }
+        }
+        return titleid;
+    }
 
     //get all reagents
-    public static ArrayList<ReagentModel> getAllReagents(){
+    public static ArrayList<Reagent> getAllReagents(){
         return reagents;
     }
 
     //add reagents
-    public static void addReagent(ReagentModel reagent){
+    public static void addReagent(Reagent reagent){
         reagents.add(reagent);
     }
 
     //get reagents by id
 
+    public static Reagent getReagentById(int id){
+
+        Reagent chemicalID = null;
+
+        for (Reagent theChemical : reagents){
+            if ( theChemical.getChemID() == id){
+                chemicalID = theChemical;
+            }
+        }
+        return chemicalID;
+    }
 
     //remove reagent
 
+    public static void removeReagent(int id){
+
+        Reagent chemicalToRemove = getReagentById(id);
+        reagents.remove(chemicalToRemove);
+    }
+
      //add procedure and observations
-    public static void addProcObs(ProcObsModel procob){
+    public static void addProcObs(ProcObs procob){
         procobs.add(procob);
     }
     //getall
 
-    public static ArrayList<ProcObsModel> getAllProcObs(){
+    public static ArrayList<ProcObs> getAllProcObs(){
         return procobs;
     }
     //remove
 
     //add
-    public static void addConclusion(ConclusionModel conclusion){
+    public static void addConclusion(Conclusion conclusion){
         conclusions.add(conclusion);
     }
     //get all conclusions
-    public static ArrayList<ConclusionModel> getAllConclusions(){
+    public static ArrayList<Conclusion> getAllConclusions(){
         return conclusions;
     }
     //remove
