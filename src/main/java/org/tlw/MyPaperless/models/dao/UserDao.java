03a41ccc5bbf03e3cf8a0a6c4f39549b2199eb8c
TaNeisha Wilks <twilks18@gmail.com>
2018-01-06 12:51:53 -0600

    Uncommitted files. Use insted of "Added AbstractModel"

---
+++ src/main/java/org/tlw/MyPaperless/models/dao/UserDao.java
@@ -0,0 +1,20 @@
+package org.tlw.MyPaperless.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.tlw.MyPaperless.models.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface UserDao extends CrudRepository<User, Integer >{
+
+     List<User> findAll();
+
+     User findByUid(Integer uid);
+
+     List<User> findByName(String name);
+
+}
